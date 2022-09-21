eeaf01a6bb8bef11ef11c07fe0d1f1bfcda29ad6
Christoph Pranzl <christoph.pranzl@pranzl.net>
2022-09-21 09:49:15 +0200

    Applying PEP8 guidelines

--- setup.py
+++ setup.py
@@ -8,18 +8,21 @@ from setuptools import _install_setup_requires, setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "mfrc522_i2c",
-    version = "0.0.3",
-    author = "Christoph Pranzl",
-    author_email = "christoph.pranzl@pranzl.net",
-    description = ("MFRC522 RFID reader/writer I2C driver in Python 3"),
-    license = "GPLv3",
-    keywords = "i2c rfid mfrc522",
-    url = "https://github.com/cpranzl/mfrc522_i2c",
+    name="mfrc522_i2c",
+    version="0.0.3",
+    author="Christoph Pranzl",
+    author_email="christoph.pranzl@pranzl.net",
+    description=("MFRC522 RFID reader/writer I2C driver in Python 3"),
+    license="GPLv3",
+    keywords="i2c rfid mfrc522",
+    url="https://github.com/cpranzl/mfrc522_i2c",
     packages=['mfrc522_i2c'],
     long_description=read('README.md'),
     classifiers=[
@@ -29,7 +32,7 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
     ],
-    _install_requires=[
+    install_requires=[
         "smbus==1.1.post2"
     ]
 )
